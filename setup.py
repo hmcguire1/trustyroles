b704e9c42e41fd3e2f90b90db67d7072fe8d2d62
hmcguire1 <hmcguire8621@gmail.com>
2019-11-29 09:28:39 -0500

    Adding packaging setup.py and new directory structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='trustyroles',
+    version='0.0.2',
+    description='AWS roles toolkit',
+    packages=setuptools.find_packages(),
+    url='https://github.com/hmcguire1/trustyroles',
+    author_email='hmcguire8621@gmail.com',
+    author='hmcguire1',
+    install_requires=[
+        'boto3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['ardp_update=trustyroles.arpd_update:_main'],
+    },
+    python_requires='>=3.6'
+)
