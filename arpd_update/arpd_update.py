68cb7299a4cec9e3e795118a79e2982d218c8820
hmcguire1 <hmcguire8621@gmail.com>
2019-12-03 21:08:22 -0800

    Added error handling for iam api update calls.

--- arpd_update/arpd_update.py
+++ arpd_update/arpd_update.py
@@ -2,6 +2,7 @@ import json
 import logging
 import argparse
 import boto3
+from botocore.exceptions import ClientError
 
 LOGGER = logging.getLogger('IAM-ROLE-TRUST-POLICY')
 logging.basicConfig(level=logging.WARNING)
@@ -28,15 +29,15 @@ def _main():
         '-m', '--method',
         type=str,
         required=False,
-        choices=['update', 'remove', 'get'],
-        help='Takes choice of method to update, get, or remove.'
+        choices=['get', 'update', 'remove'],
+        help='Takes choice of method to get, update, or remove.'
     )
 
     PARSER.add_argument(
         '-e', '--add_external_id',
         type=str,
         required=False,
-        help='Takes an external id as a string.'
+        help='Takes an externalId as a string.'
     )
 
     PARSER.add_argument(
@@ -46,7 +47,7 @@ def _main():
     )
 
     PARSER.add_argument(
-        '-j', '--json_flag',
+        '-j', '--json',
         action='store_true',
         required=False,
         help='Add to print json in get method.'
@@ -116,12 +117,16 @@ def add_external_id(external_id, role_name):
     else:
         ardp['Statement'][0]['Condition'] = {'StringEquals': {'sts:ExternalId': external_id}}
 
-    iam_client.update_assume_role_policy(
-        RoleName=role_name,
-        PolicyDocument=json.dumps(ardp)
-    )
+    try:
+        iam_client.update_assume_role_policy(
+            RoleName=role_name,
+            PolicyDocument=json.dumps(ardp)
+        )
+    except ClientError as error:
+        print(error)
+
 def remove_external_id(role_name):
-    """The remove_external_id method takes an external_id and role_name as strings
+    """The remove_external_id method takes a role_name as a string
         to allow the removal of an externalId condition."""
     iam_client = boto3.client('iam')
     role = iam_client.get_role(RoleName=role_name)
@@ -129,10 +134,14 @@ def remove_external_id(role_name):
 
     if ardp['Statement'][0]['Condition'] is not None:
         ardp['Statement'][0]['Condition'] = {}
-        iam_client.update_assume_role_policy(
-            RoleName=role_name,
-            PolicyDocument=json.dumps(ardp)
-        )
+
+        try:
+            iam_client.update_assume_role_policy(
+                RoleName=role_name,
+                PolicyDocument=json.dumps(ardp)
+            )
+        except ClientError as error:
+            print(error)
 
 def update_arn(arn_list, role_name):
     """The update_arn method takes a list of ARNS(arn_list) and a role_name
@@ -149,40 +158,43 @@ def update_arn(arn_list, role_name):
                 ardp['Statement'][0]['Principal']['AWS'] = old_principal_list
             else:
                 new_principal_list = []
-                for arn_item in arn_list:
-                    new_principal_list.append(arn_item)
+                for arn in arn_list:
+                    new_principal_list.append(arn)
                 new_principal_list.append(old_principal_list)
                 ardp['Statement'][0]['Principal']['AWS'] = new_principal_list
 
-    for arn in arn_list:
-        LOGGER.info("Updating Policy to add: '%s'", arn)
-
-    iam_client.update_assume_role_policy(
-        RoleName=role_name,
-        PolicyDocument=json.dumps(ardp)
-    )
+    try:
+        iam_client.update_assume_role_policy(
+            RoleName=role_name,
+            PolicyDocument=json.dumps(ardp)
+        )
+    except ClientError as error:
+        print(error)
 
 def remove_arn(arn_list, role_name):
-    """The remove_arn method takes in a list of ARNS(arn_list) and a role_name
+    """The remove_arn method takes in a string or list of ARNs and a role_name
         to remove ARNS from trust policy of supplied role."""
     iam_client = boto3.client('iam')
     role = iam_client.get_role(RoleName=role_name)
     ardp = role['Role']['AssumeRolePolicyDocument']
     old_principal_list = ardp['Statement'][0]['Principal']['AWS']
 
-    for arn in arn_list:
-        if arn in old_principal_list:
-            old_principal_list.remove(arn)
+    if isinstance(arn_list, list):
+        for arn in arn_list:
+            if arn in old_principal_list:
+                old_principal_list.remove(arn)
+    else:
+        old_principal_list.remove(arn_list)
 
     ardp['Statement'][0]['Principal']['AWS'] = old_principal_list
 
-    for arn in arn_list:
-        LOGGER.info("Updating Policy to remove: '%s'", arn)
-
-    iam_client.update_assume_role_policy(
-        RoleName=role_name,
-        PolicyDocument=json.dumps(ardp)
-    )
+    try:
+        iam_client.update_assume_role_policy(
+            RoleName=role_name,
+            PolicyDocument=json.dumps(ardp)
+        )
+    except ClientError as error:
+        print(error)
 
 if __name__ == "__main__":
     _main()
